51f4ee4e807c2fa756719d9fe39f5e79f5df62c5
Trevor Johst <t_edword@aol.com>
2023-01-25 12:18:24 -0500

    Basic orbital animation implemented

--- OrbitVisualization/Environment.py
+++ OrbitVisualization/Environment.py
@@ -1,27 +1,42 @@
+from skyfield.api import EarthSatellite
+from skyfield.api import load
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from datetime import timedelta
 import numpy as np
 
-class Planet:
-    def __init__(self, radius, env_width, grid=False, wireframe=False, darkmode=True):
+class Environment:
+    def __init__(self, radius, env_width, start_time, duration=None, grid=False, darkmode=True):
 
         # Object variables
-        self.radius = radius
-        self.env_width = env_width
+        self.satellites = []
+        self.ts = load.timescale()
+        self.start_time = start_time
+        self.duration = duration
+        if duration: 
+            self.end_time = self.ts.utc(start_time.utc_datetime() + timedelta(hours=duration)) 
+        else: 
+            self.end_time = None
         
         # Colors
         linecolor = 'black'
         dark = '#181818'
-        planet = '#2B4EFF'
 
         # Create figure and set parameters
         self.fig = plt.figure()
-        ax = self.fig.add_subplot(projection='3d')
+        self.ax = self.fig.add_subplot(projection='3d')
+        plt.locator_params(nbins=3)
 
-        ax.set_box_aspect([1,1,0.9])
+        self.ax.set_box_aspect([1,1,0.9])
+        self.ax.view_init(10, 45)
 
-        ax.xaxis.pane.fill = False
-        ax.yaxis.pane.fill = False
-        ax.zaxis.pane.fill = False
+        self.ax.set_xlim(-env_width, env_width)
+        self.ax.set_ylim(-env_width, env_width)
+        self.ax.set_zlim(-env_width, env_width)
+
+        self.ax.xaxis.pane.fill = False
+        self.ax.yaxis.pane.fill = False
+        self.ax.zaxis.pane.fill = False
 
         # Grid handling
         if not grid:
@@ -30,27 +45,79 @@ class Planet:
         # Darkmode handling
         if darkmode:   
             linecolor = 'white'
-            planet = '#162780'
 
             self.fig.patch.set_facecolor(dark)
-            ax.set_facecolor(dark)
+            self.ax.set_facecolor(dark)
 
             if grid:
-                ax.w_xaxis.line.set_color('white')
-                ax.w_yaxis.line.set_color('white')
-                ax.w_zaxis.line.set_color('white')
+                self.ax.w_xaxis.line.set_color('white')
+                self.ax.w_yaxis.line.set_color('white')
+                self.ax.w_zaxis.line.set_color('white')
 
-                ax.tick_params(colors='white')
+                self.ax.tick_params(colors='white')
             
-        # Generate sphere graphing data
+        # Generate and graph sphere
         theta, phi = np.mgrid[0:2*np.pi:20j, 0:np.pi:20j]
 
         x = radius*np.cos(theta)*np.sin(phi)
         y = radius*np.sin(theta)*np.sin(phi)
         z = radius*np.cos(phi)
 
-        # Wireframe handling
-        if wireframe:
-            ax.plot_wireframe(x, y, z, color=linecolor, linewidth=0.5)
-        else:
-            ax.plot_surface(x, y, z, color=planet, edgecolor=linecolor, linewidth=0.5)
+        self.ax.plot_wireframe(x, y, z, color=linecolor, linewidth=0.5)
+
+    def updateSat(self, t, sat_plots):
+
+        for i, sat in enumerate(self.satellites):
+            
+            # Get coordinates of satellite at current time
+            x, y, z =  self.satellites[i].at(t).position.km
+            
+            # Update satellite plot's position on graph
+            sat_plots[i].set_data([x], [y])
+            sat_plots[i].set_3d_properties([z])
+            
+
+    def addSatellite(self, TLE):
+
+        # Add satellite to the list
+        self.satellites.append(EarthSatellite(TLE.line1, TLE.line2, name=TLE.name))
+
+    def animate(self, save=False):
+
+        # Ensure the environment is initialized for animation
+        if not self.duration:
+            raise RuntimeError("Cannot animate without a duration.")
+
+        # Generate time array
+        t = self.ts.linspace(self.start_time, self.end_time, self.duration*100)
+        
+        sat_plots = []
+
+        for i, sat in enumerate(self.satellites):
+
+            # Plot orbital path
+            pos = sat.at(t).position.km
+            x, y, z = pos
+            self.ax.plot(x, y, z)
+
+            # Create plot for satellite
+            sat_plot, = self.ax.plot(0, 1, marker="o")
+            sat_plots.append(sat_plot)
+        
+        # Create animation
+        anim = animation.FuncAnimation(self.fig, self.updateSat, interval=70, repeat=True, 
+                                                      frames=t, fargs=(sat_plots,), save_count=3000)
+
+        # Save the animation if desired
+        if save:
+            f = r"c://Users/User/Desktop/animation.gif" 
+            writergif = animation.PillowWriter(fps=15) 
+            anim.save(f, writer=writergif)
+
+        # Show the animation
+        plt.show()
+
+
+    def debug(self):
+        plt.show()
+
--- OrbitVisualization/OrbitVisualization.py
+++ OrbitVisualization/OrbitVisualization.py
@@ -1,7 +1,47 @@
-import Environment
+from Environment import Environment
+from skyfield.api import load
 
-def main():
-    Earth = Environment.Planet(6371, 10000, grid=False, wireframe=False, darkmode=True)
+class TLE:
+    def __init__(self, TLE):
+
+        # Extract TLE lines according to file formatting
+        lines = TLE.splitlines()
+
+        if len(lines) == 3:
+            self.name = lines[0]
+            self.line1 = lines[1]
+            self.line2 = lines[2]
+        elif len(lines) == 2:
+            self.name = None
+            self.line1 = lines[0]
+            self.line2 = lines[1]
+        else:
+            raise ValueError
+
+    def __str__(self):
+
+        # Build output string
+        output = ""
+
+        if self.name:
+            output += self.name + "\n"
+        output += self.line1 + "\n"
+        output += self.line2
+
+        return output
+
 
 if __name__ == "__main__":
-    main()
+    test = TLE("""NEXTSAT-1
+1 43811U 18099BF  23017.44801183  .00004536  00000+0  39757-3 0  9997
+2 43811  97.5949  83.7508 0011279 267.0241  92.9691 14.97097622224635""")
+    test2 = TLE("""STARLINK-3106
+1 49164U 21082AL  23017.70091959  .00023448  00000+0  19664-2 0  9998
+2 49164  70.0020 286.4184 0003135 278.2144  81.8659 14.98329354 75836""")
+    
+    ts = load.timescale()
+    
+    Earth = Environment(6371, 8000, ts.utc(2023, 1, 10), duration=2, grid=False, darkmode=True)
+    Earth.addSatellite(test)
+    Earth.addSatellite(test2)
+    Earth.animate(save=True)
